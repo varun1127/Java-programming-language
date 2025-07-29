a88262cfe244577ca18af70944921010cafb60b7
varun1127 <choudhryvarun370@gmail.comgit config --global user.email choudhryvarun370@gmail.in>
2025-07-30 03:57:12 +0530

    STRINGS.java/compression.class

---
+++ STRINGS.java/largeststr.java
@@ -0,0 +1,13 @@
+public class largeststr {
+    public static void main(String args[]) {
+        String fruits[] = {"Apple", "Banana", "Grapes"};
+
+        String largest = fruits[0];
+        for(int i=1; i<fruits.length; i++) {
+            if(largest.compareTo(fruits[i])<0) {
+                largest = fruits[i];
+            }
+        }
+        System.out.println(largest);
+    }
+}
